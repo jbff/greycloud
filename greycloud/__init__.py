0eba14cab32cb369a563f268118685063e682a66
John B. Fleming <jbfleming@gmail.com>
2026-02-10 01:36:26 -0600

    chore: fix __version__ to 0.3.2 for release

--- greycloud/__init__.py
+++ greycloud/__init__.py
@@ -13,7 +13,7 @@ from .cache import GreyCloudCache
 from .rate_limiter import VertexRateLimiter
 from .async_client import GreyCloudAsyncClient
 
-__version__ = "0.3.3"
+__version__ = "0.3.2"
 __all__ = [
     "GreyCloudConfig",
     "GreyCloudClient",
